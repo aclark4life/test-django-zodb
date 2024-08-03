eb9dfc28fc5829fb152c911788238cbabd803940
Jeffrey Alex Clark <aclark@aclark.net>
2024-08-03 18:14:00 -0400

    Update test-django-zodb

--- project.mk
+++ project.mk
@@ -4,4 +4,4 @@ clean:
 	rm -rvf ../../mongodb/django-zodb/build/
 
 edit:
-	nvim ../../mongodb/django-zodb/django_zodb/base.py ../../mongodb/django-zodb/django_zodb/operations.py
+	nvim ../../mongodb/django-zodb/django_zodb/base.py ../../mongodb/django-zodb/django_zodb/introspection.py
