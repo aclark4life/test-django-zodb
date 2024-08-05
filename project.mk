c527aa3d98947886f99453e607b613a41ce4efb2
Jeffrey Alex Clark <aclark@aclark.net>
2024-08-05 07:16:10 -0400

    Update test-django-zodb

--- project.mk
+++ project.mk
@@ -1,13 +1,4 @@
 PROJECT_NAME := test-django-zodb
 
-clean:
-	rm -rvf ../../mongodb/django-zodb/build/
-
 edit:
-	nvim ../../mongodb/django-zodb/django_zodb/base.py
-
-shell:
-	python shell.py
-
-diff:
-	cd ../../mongodb/django-zodb/ && git diff
+	nvim .venv/src/django-zodb/django_zodb/base.py
